68faf9d091e5d2704ecbf158104c3e4ee218c8fe
Paladin <h.m4571@gmail.com>
2022-11-13 16:46:45 +0330

    Complete Task 2

--- express_sequelize/src/Modules/events/events.service.ts
+++ express_sequelize/src/Modules/events/events.service.ts
@@ -1,7 +1,6 @@
 import Event from './entities/event.entity';
 import Workshop from './entities/workshop.entity';
-
-
+const { Op } = require('sequelize')
 export class EventsService {
 
   async getWarmupEvents() {
@@ -13,74 +12,20 @@ export class EventsService {
       include: [{model: Workshop,required: true,order:[["id","asc"]]}]
     });           
   }
-
-  /* TODO: complete getFutureEventWithWorkshops so that it returns events with workshops, that have not yet started
-    Requirements:
-    - only events that have not yet started should be included
-    - the event starting time is determined by the first workshop of the event
-    - the code should result in maximum 3 SQL queries, no matter the amount of events
-    - all filtering of records should happen in the database
-    - verify your solution with `npm run test`
-    - do a `git commit && git push` after you are done or when the time limit is over
-    - Don't post process query result in javascript
-    Hints:
-    - open the `src/events/events.service.ts` file
-    - partial or not working answers also get graded so make sure you commit what you have
-    - join, whereIn, min, groupBy, havingRaw might be helpful
-    - in the sample data set  the event with id 1 is already in the past and should therefore be excluded
-    Sample response on GET /futureevents:
-    ```json
-    [
-        {
-            "id": 2,
-            "name": "Laravel convention 2023",
-            "createdAt": "2023-04-20T07:01:14.000000Z",
-            "workshops": [
-                {
-                    "id": 2,
-                    "start": "2023-10-21 10:00:00",
-                    "end": "2023-10-21 18:00:00",
-                    "eventId": 2,
-                    "name": "The new Eloquent - load more with less",
-                    "createdAt": "2021-04-20T07:01:14.000000Z",
-                },
-                {
-                    "id": 3,
-                    "start": "2023-11-21 09:00:00",
-                    "end": "2023-11-21 17:00:00",
-                    "eventId": 2,
-                    "name": "AutoEx - handles exceptions 100% automatic",
-                    "createdAt": "2021-04-20T07:01:14.000000Z",
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "name": "React convention 2023",
-            "createdAt": "2023-04-20T07:01:14.000000Z",
-            "workshops": [
-                {
-                    "id": 4,
-                    "start": "2023-08-21 10:00:00",
-                    "end": "2023-08-21 18:00:00",
-                    "eventId": 3,
-                    "name": "#NoClass pure functional programming",
-                    "createdAt": "2021-04-20T07:01:14.000000Z",
-                },
-                {
-                    "id": 5,
-                    "start": "2023-08-21 09:00:00",
-                    "end": "2023-08-21 17:00:00",
-                    "eventId": 3,
-                    "name": "Navigating the function jungle",
-                    "createdAt": "2021-04-20T07:01:14.000000Z",
-                }
-            ]
-        }
-    ]
-    ```
-     */
-  async getFutureEventWithWorkshops() {
-    throw new Error('TODO task 2');
+  
+  async getFutureEventWithWorkshops() {   
+    console.log(Date.now());
+      return await Event.findAll({
+        include: [{
+          model: Workshop,
+          required: true,
+          where:{
+            start: {
+              [Op.gt]: new Date()
+            }
+          },
+          order:[["id","asc"]]
+        }]
+      });  
   }
 }
